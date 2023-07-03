f9bb7d702884484454cbfc29baced5768cf41de4
AlexBAlcantara14 <135645103+AlexBAlcantara14@users.noreply.github.com>
2023-07-03 20:18:57 +0000

    EC2-BANCES

---
+++ src/main/java/com/example/ec2bances/Ec2BancesApplication.java
@@ -0,0 +1,13 @@
+package com.example.ec2bances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2BancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec2BancesApplication.class, args);
+	}
+
+}
