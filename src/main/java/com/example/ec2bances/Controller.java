4854bc55cbcd8ca28f34252ab8531488533e8bc2
AlexBAlcantara14 <135645103+AlexBAlcantara14@users.noreply.github.com>
2023-07-03 21:08:51 +0000

    EC2-BANCES2

--- src/main/java/com/example/ec2bances/Controller.java
+++ src/main/java/com/example/ec2bances/Controller.java
@@ -1,15 +1,25 @@
 package com.example.ec2bances;
 
+
 import java.lang.String;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController 
-@RequestMapping(path="/")
+@RestController
+@RequestMapping(path="/") //link principal padre
 public class Controller{
-    @GetMapping(path= "/")
-    public String home(){
-        return "PT71467042 - Bances Alcantara Alexis";
-    }
+    //link segundo hijo
+        @GetMapping(path="/") 
+        public String home(){
+            return "PT71467042 - Alexis Bances Alcantara";
+        }
+        @GetMapping(path="/idat/codigo") 
+        public String codigo(){
+            return "PT71467042";
+        }
+        @GetMapping(path="/idat/nombre") 
+        public String nombrecompleto(){
+            return "Alexis Bances Alcantara";
+        }
 }
